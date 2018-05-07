4aaad9a584c08a2c749a2eb89f012a9418831784
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2018-05-07 15:55:10 -0500

    v3

--- vista.py
+++ vista.py
@@ -1,31 +1,52 @@
-def hacerRegistro(self):
+def hacerRegistro():
 	print("Registrese:")
 	nombre = str(input("Ingrese su nombre: "))
 	apellidos = str(input("Ingrese sus apellidos:"))
 	correo = str(input("Ingrese su correo: "))
 	password = str(input("Ingrese su contraseña nueva: "))
 	edad = int(input("Ingrese su edad: "))
+	sexo = bool(input("Ingrese el sexo que es usted: "))
 	telefono = str(input("Ingrese su numero de telefono: "))
 	if edad <= 18:
 		print("Usuario ", nombre, " usted no puede crear su cuenta por que es menor de 18 años")
 	else:
-		print("Bienvenido ", nombre, " ")
+		print("Bienvenido ", nombre)
 		#metodo para guardar los datos del regitro
 
-
-def inciarSesion(self):
+def inciarSesion():
 	print("Incie sesion")
 	usuario = str(input("Ingrese "))
 
-print("""
 
-\t----------------------------------------------------------------------------
-	Red Social de Eventos OpenWorld
-	1) Realizar registro
-	2) Inciar Sesion
-	3) Salir
-	""")
-print()
-confirmacion = int(input("Ingrese su opcion: "))
+# esto mostrar el menu principal
+
+def menuPrincipal():
+	print("""
+	
+	\t--------------------------------------------------------
+	\t|	 Red Social de Eventos OpenWorld               |
+	\t|	 1) Realizar registro                          |
+	\t|	 2) Inciar Sesion                              |
+	\t|	 3) Salir                                      |
+	\t|                                                      |
+	\t|                                                      |
+	\t--------------------------------------------------------
+		""")
+	print()
+
+if __name__ == '__main__':
+
+	menuPrincipal()
+
+	confirmacion = int(input("\tIngrese su opcion: "))
+	if confirmacion == 1:
+		hacerRegistro()
+	elif confirmacion == 2:
+		inciarSesion()
+	elif confirmacion == 3:
+		print("Usted salio")
+	else:
+		menuPrincipal()
+# aqui termina el menu principal
 
 	
